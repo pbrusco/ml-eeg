5d1be6823dafe1518e053cf85db9f13efc42b17f
Pablo Brusco <pablo.brusco@gmail.com>
2017-04-17 09:48:30 -0300

    version upgraded

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='17.04.01',
+    version='17.04.02',
 
     description='A ml & eeg helper library',
     long_description=long_description,
