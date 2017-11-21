5b50fcbeca75e2c5676d8649e208d5b7f0ad6f96
Pablo Brusco <pablo.brusco@gmail.com>
2017-11-21 13:07:53 -0300

    extractor imports inplace

--- ml/feature_extraction/extractors.py
+++ ml/feature_extraction/extractors.py
@@ -1,11 +1,6 @@
 # coding=utf-8
 
-import ml.feature_extraction.eeg.windowed_extractor
-import ml.feature_extraction.eeg.raw_extractor
-import ml.feature_extraction.eeg.freq_extractor
-import ml.feature_extraction.eeg.trash_extractor
 
-import ml.feature_extraction.audio.acoustics_extractor
 
 from .. import utils
 
@@ -13,14 +8,19 @@ from .. import utils
 def create(config):
     params = utils.read_config(config)
     if params["extraction_method"] == "raw":
+        import ml.feature_extraction.eeg.raw_extractor
         return ml.feature_extraction.eeg.raw_extractor.RawExtractor(params)
     elif params["extraction_method"] == "freqs":
+        import ml.feature_extraction.eeg.freq_extractor
         return ml.feature_extraction.eeg.freq_extractor.FreqExtractor(params)
     elif params["extraction_method"] == "trash":
+        import ml.feature_extraction.eeg.trash_extractor
         return ml.feature_extraction.eeg.trash_extractor.TrashExtractor(params)
     elif params["extraction_method"] == "window_mean":
+        import ml.feature_extraction.eeg.windowed_extractor
         return ml.feature_extraction.eeg.windowed_extractor.WindowedExtractor(params)
     elif params["extraction_method"] == "wavelets":
         return ml.feature_extraction.eeg.wavelets_extractor.WaveletsExtractor(params)
     elif params["extraction_method"] == "acustics":
+        import ml.feature_extraction.audio.acoustics_extractor
         return ml.feature_extraction.audio.acoustics_extractor.AcousticsExtractor(params)
