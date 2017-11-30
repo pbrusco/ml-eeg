36ace1b3b32a457c0af009a8019cc06acb2d5c19
Pablo Brusco <pablo.brusco@gmail.com>
2017-11-30 17:57:07 -0300

    mask updated

--- ml/data_processing.py
+++ ml/data_processing.py
@@ -18,22 +18,23 @@ def handle_nans(nan_action, X, features_names):
     if nan_action in ["replace_by_zero", "replace_by_mean", "replace_by_median"]:
         X = np.hstack([X, np.isnan(X) * 1.0])
         features_names = features_names + ["was_nan" + f for f in features_names]
+        deleted_rows = np.zeros(X.shape[0], np.bool)
 
     if nan_action == "replace_by_zero":
         print(("converting {} NaN to 0s".format(np.count_nonzero(~np.isnan(X)))))
         X = np.nan_to_num(X)
-        deleted_rows = np.array([])
+        deleted_rows = np.zeros(X.shape[0], np.bool)
 
     if nan_action == "replace_by_mean":
         col_mean = np.nanmean(X, axis=0)
         inds = np.where(np.isnan(X))
         X[inds] = np.take(col_mean, inds[1])
-        deleted_rows = np.array([])
+        deleted_rows = np.zeros(X.shape[0], np.bool)
 
     if nan_action == "replace_by_median":
         col_mean = np.nanmedian(X, axis=0)
         inds = np.where(np.isnan(X))
         X[inds] = np.take(col_mean, inds[1])
-        deleted_rows = np.array([])
+        deleted_rows = np.zeros(X.shape[0], np.bool)
 
     return X, deleted_rows, features_names
