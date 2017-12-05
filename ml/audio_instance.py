0f1e23592bf0462b86aa6aed4d2bcdcb2908ea37
Pablo Brusco <pablo.brusco@gmail.com>
2017-12-05 10:49:44 -0300

    audio instance

--- ml/audio_instance.py
+++ ml/audio_instance.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from . import system
+from . import signal_processing
 
 
 class AudioInstance:
-    def __init__(self, filename, audio, speaker_gender):
+    def __init__(self, filename, speaker_gender):
         self.filename = filename
-        self.audio = audio
+        self.audio = signal_processing.load_wav(filename)
         self.gender = speaker_gender.lower().strip()
         assert system.exists(filename), "filename {} does not exist"
         assert self.gender in ["m", "f"], "gender options are 'm' or 'f'"
